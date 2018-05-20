91ccbe63fe789e842d507c4ce62718414af470f9
TPFChaos <thiagoiferreira@outlook.com>
2018-05-20 13:11:58 -0300

    Starting puma backend spring project

---
+++ src/main/java/com/puma/puma/PumaApplication.java
@@ -0,0 +1,12 @@
+package com.puma.puma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/puma/puma/PumaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.puma.puma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PumaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
